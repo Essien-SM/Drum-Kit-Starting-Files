1b3825c03b180e0b41c443536f34d4422bef0b47
Essien-SM <stephenyessien@gmail.com>
2023-05-03 22:19:42 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var numberOfButton = document.querySelectorAll(".drum").length
+
+for (var i = 0; i < numberOfButton; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("I got clicked")
+    })
+}
